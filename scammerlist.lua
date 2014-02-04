59afb50a1f665d5d881e5acc7eac695d0c69b208
Leonardo Falk <leonardo.falk@hotmail.com>
2014-02-04 20:16:20 -0200

    Update scammerlist.lua

--- scammerlist.lua
+++ scammerlist.lua
@@ -1,89 +1,80 @@
 SCAM_LIST = {
-	{name = "Joao Carlos Brinquete", adresses = "skype: joao.carlos.brinquete.avo\nemail: joaowbrinquete@hotmail.com\nemail: joaoow@outlook.pt", nicks = "joaoow, iJoaoW", reasons = "Stole 4 Tibia Premium Time"},
-	{name = "Kelli Deferiese", adresses = "email: kdefriese@charter.net\nemail: kdefriese@charter.net", nicks = "-", reasons = "Known Scammer"},
-	{name = "Rosa Maria Leal Freitas", adresses = "email: rosafreitas@globo.com\nemail:rosafreitas@globo.com", nicks = "-", reasons = "Known Scammer"},
-	{name = "Carlos Mar", adresses = "email: silvadomar@globo.com\nemail: silvadomar@globo.com", nicks = "-", reasons = "Known Scammer"},
-	{name = "Marco Ruellas", adresses = "email: ruellas@yahoo.com.br\nemail: ruellas@yahoo.com.br", nicks = "-", reasons = "Known Scammer"},
-	{name = "Laura Jones", adresses = "email: lawa1985@live.com\nskype: tibia.buyer", nicks = "Flamed", reasons = "Known Scammer"},
-	{name = "Matthew Zwicker", adresses = "email: matthewmaclean2000@hotmail.com\nskype: Matthew MacLean", nicks = "Sir Rexy", reasons = "Known Scammer"},
-	{name = "Emil Mårtensson", adresses = "email: Oscaroggejohansson@hotmail.com", nicks = "Characterforsale", reasons = "Known Scammer"},
-	{name = "Jamil Messias Sales", adresses = "email: jms.bel@gmail.com -", nicks = "-", reasons = "Known Scammer"},
-	{name = "-", adresses = "skype: corphia1\nskype: deidara.1990\nskype: myoncharacters\nemail: solantes12@hotmail.com\nskype: exef.exef", nicks = "corpie, corphek, corphia, Deidara, muooo, Solantes, exefexef, exef.exef, freeusertest1, adi101111, Sonik, exefik, corpha, corphia1", reasons = "Known Scammer"},
-	{name = "Valdecir J. Sousa, Jose De Sousa", adresses = "email: vev.valdecirjs@gmail.com", nicks = "-", reasons = "Known Scammer"},
-	{name = "Jaroslaw Sas", adresses = "skype: Annacugierkotka\nemail: neobot111@o2.pl", nicks = "amel13141", reasons = "Known Scammer"},
-	{name = "Rodrigo Molinari", adresses = "skype: rodrigomolinari2", nicks = "81432858", reasons = "Known Scammer"},
-	{name = "John Barkol", adresses = "email: holembik@gmail.com\nskype: john.barkol", nicks = "dexik39", reasons = "Known Scammer"},
-	{name = "JORGE R JACOB FH", adresses = "email: jorgejacob@gmail.com", nicks = "leonardogomes", reasons = "Known Scammer"},
-	{name = "-", adresses = "skype: alexis.darks", nicks = "alexisdarks", reasons = "Known Scammer"},
-	{name = "-", adresses = "skype: daniel.alexis38", nicks = "alexisdarks", reasons = "Known Scammer"},
-	{name = "Kevin Chau", adresses = "email: kevin_chau9506@hotmail.com", nicks = "HeHao", reasons = "Known Scammer"},
-	{name = "Cameron Kieffer", adresses = "email: sdeisherlpn2011@hotmail.com, kieffer1230", nicks = "xXKEFFERXx", reasons = "Known Scammer"},
-	{name = "David Sadler", adresses = "email: davidsadler47@live.com\nskype: williamsjake", nicks = "Azleh", reasons = "Known Scammer"},
-	{name = "Kevin-Raussche", adresses = "email: rausche.kevin@googlemail.com\nskype: maciej.czoger\nskype: tibia.inc\nskype: tim.sukovski", nicks = "czoger, Tibiainc, Moneyrainempire", reasons = "Known Scammer"},
-	{name = "Stig Heimro", adresses = "email: stighiemro@hotmail.com\nemail: stighiemro@hotmail.com", nicks = "-", reasons = "Known Scammer"},
-	{name = "Phil (Philip) McCafferty", adresses = "skype: Olney\nskype: Illinois\nskype: USAownageforhire\nemail: Mainstreamdarkness@gmail.com\nemail: Frostwynd.tibia@gmail.com", nicks = "ungoliant", reasons = "Known Scammer"},
-	{name = "Hazem Shaba", adresses = "email: bassam.shaba@hotmail.se\nskype: Bassehunt", nicks = "nachoo123", reasons = "Known Scammer"},
-	{name = "Alex Wilkes", adresses = "email: wilks3y@hotmail.co.uk\nskype: iwilks3y", nicks = "wilks3y", reasons = "Known Scammer"},
-	{name = "-", adresses = "-", nicks = "Unit", reasons = "Known Scammer"},
-	{name = "Kurdiskop", adresses = "skype: kurdiskop.op1", nicks = "Farbror Kurd III", reasons = "Known Scammer"},
-	{name = "Emerson-Queiroz", adresses = "email: Emersontrk@yahoo.com.bremersontrk", nicks = "emersontrk", reasons = "Known Scammer"},
-	{name = "William Steen", adresses = "email: Willesteen@live.se\nskype: Wille-steen97", nicks = "Wirre", reasons = "Known Scammer"},
-	{name = "Johan Kempe", adresses = "email: jkfan.xd@hotmail.com\nskype: Johaanftw", nicks = "Johaanftw", reasons = "Known Scammer"},
-	{name = "Allex Isaksson", adresses = "email: allexallexallex@live.com\nskype: Forgiven1337", nicks = "Jah", reasons = "Known Scammer"},
-	{name = "-", adresses = "email: czarodziej.czarek@hotmail.com\nskype: czarodziej.czarek", nicks = "Czarek Czarodziej", reasons = "Known Scammer"},
-	{name = "Elizabeth DuBois", adresses = "email: eldubois@gmail.com\nskype: perdo.paulo2", nicks = "Nabirinight", reasons = "Known Scammer"},
-	{name = "-", adresses = "email: Securasolantes12@hotmail.com\nskype: solantes\nskype: Corphia1", nicks = "Solanek, qextibia, corphek, Joe, Exef", reasons = "Known Scammer"},
-	{name = "Henrik Holmgren", adresses = "skype: annesta5\nskype: John123\nskype: Lassermannez", nicks = "-", reasons = "Known Scammer"},
-	{name = "Kamil", adresses = "email: Kamilowsky1337@wp.pl\nskype: SEBEKPAN\nskype: Sbikovvy", nicks = "-", reasons = "Known Scammer"},
-	{name = "-", adresses = "email: turbooman@hotmail.com\nskype: Liveturbooman", nicks = "TurboMan", reasons = "Known Scammer"},
-	{name = "Dominik", adresses = "email: Aerozzcharacters@gmail.com\nskype: dominik10102", nicks = "Aerozz", reasons = "Known Scammer"},
-	{name = "David Johansson", adresses = "email: David.1338@live.se", nicks = "Wantox", reasons = "Known Scammer"},
-	{name = "Marian Puk", adresses = "email: marian1953@gmail.com\nemail: oskar666@outlook.com", nicks = "Oskar", reasons = "Known Scammer"},
-	{name = "Michal Czernecki", adresses = "email: mic.acc@hotmail.com\nsite: http//tibian-gold.weebly.com/\nskype: mic.acc_1", nicks = "Micacc", reasons = "Known Scammer"},
+	{name = "Joao Carlos Brinquete", addresses = "skype: joao.carlos.brinquete.avo\nemail: joaowbrinquete@hotmail.com\nemail: joaoow@outlook.pt", nicks = "joaoow, iJoaoW", reasons = "Stole 4 Tibia Premium Time"},
+	{name = "Kelli Deferiese", addresses = "email: kdefriese@charter.net\nemail: kdefriese@charter.net", nicks = "-", reasons = "Known Scammer"},
+	{name = "Rosa Maria Leal Freitas", addresses = "email: rosafreitas@globo.com\nemail:rosafreitas@globo.com", nicks = "-", reasons = "Known Scammer"},
+	{name = "Carlos Mar", addresses = "email: silvadomar@globo.com\nemail: silvadomar@globo.com", nicks = "-", reasons = "Known Scammer"},
+	{name = "Marco Ruellas", addresses = "email: ruellas@yahoo.com.br\nemail: ruellas@yahoo.com.br", nicks = "-", reasons = "Known Scammer"},
+	{name = "Laura Jones", addresses = "email: lawa1985@live.com\nskype: tibia.buyer", nicks = "Flamed", reasons = "Known Scammer"},
+	{name = "Matthew Zwicker", addresses = "email: matthewmaclean2000@hotmail.com\nskype: Matthew MacLean", nicks = "Sir Rexy", reasons = "Known Scammer"},
+	{name = "Emil Mårtensson", addresses = "email: Oscaroggejohansson@hotmail.com", nicks = "Characterforsale", reasons = "Known Scammer"},
+	{name = "Jamil Messias Sales", addresses = "email: jms.bel@gmail.com -", nicks = "-", reasons = "Known Scammer"},
+	{name = "-", addresses = "skype: corphia1\nskype: deidara.1990\nskype: myoncharacters\nemail: solantes12@hotmail.com\nskype: exef.exef", nicks = "corpie, corphek, corphia, Deidara, muooo, Solantes, exefexef, exef.exef, freeusertest1, adi101111, Sonik, exefik, corpha, corphia1", reasons = "Known Scammer"},
+	{name = "Valdecir J. Sousa, Jose De Sousa", addresses = "email: vev.valdecirjs@gmail.com", nicks = "-", reasons = "Known Scammer"},
+	{name = "Jaroslaw Sas", addresses = "skype: Annacugierkotka\nemail: neobot111@o2.pl", nicks = "amel13141", reasons = "Known Scammer"},
+	{name = "Rodrigo Molinari", addresses = "skype: rodrigomolinari2", nicks = "81432858", reasons = "Known Scammer"},
+	{name = "John Barkol", addresses = "email: holembik@gmail.com\nskype: john.barkol", nicks = "dexik39", reasons = "Known Scammer"},
+	{name = "JORGE R JACOB FH", addresses = "email: jorgejacob@gmail.com", nicks = "leonardogomes", reasons = "Known Scammer"},
+	{name = "-", addresses = "skype: alexis.darks", nicks = "alexisdarks", reasons = "Known Scammer"},
+	{name = "-", addresses = "skype: daniel.alexis38", nicks = "alexisdarks", reasons = "Known Scammer"},
+	{name = "Kevin Chau", addresses = "email: kevin_chau9506@hotmail.com", nicks = "HeHao", reasons = "Known Scammer"},
+	{name = "Cameron Kieffer", addresses = "email: sdeisherlpn2011@hotmail.com, kieffer1230", nicks = "xXKEFFERXx", reasons = "Known Scammer"},
+	{name = "David Sadler", addresses = "email: davidsadler47@live.com\nskype: williamsjake", nicks = "Azleh", reasons = "Known Scammer"},
+	{name = "Kevin-Raussche", addresses = "email: rausche.kevin@googlemail.com\nskype: maciej.czoger\nskype: tibia.inc\nskype: tim.sukovski", nicks = "czoger, Tibiainc, Moneyrainempire", reasons = "Known Scammer"},
+	{name = "Stig Heimro", addresses = "email: stighiemro@hotmail.com\nemail: stighiemro@hotmail.com", nicks = "-", reasons = "Known Scammer"},
+	{name = "Phil (Philip) McCafferty", addresses = "skype: Olney\nskype: Illinois\nskype: USAownageforhire\nemail: Mainstreamdarkness@gmail.com\nemail: Frostwynd.tibia@gmail.com", nicks = "ungoliant", reasons = "Known Scammer"},
+	{name = "Hazem Shaba", addresses = "email: bassam.shaba@hotmail.se\nskype: Bassehunt", nicks = "nachoo123", reasons = "Known Scammer"},
+	{name = "Alex Wilkes", addresses = "email: wilks3y@hotmail.co.uk\nskype: iwilks3y", nicks = "wilks3y", reasons = "Known Scammer"},
+	{name = "-", addresses = "-", nicks = "Unit", reasons = "Known Scammer"},
+	{name = "Kurdiskop", addresses = "skype: kurdiskop.op1", nicks = "Farbror Kurd III", reasons = "Known Scammer"},
+	{name = "Emerson-Queiroz", addresses = "email: Emersontrk@yahoo.com.bremersontrk", nicks = "emersontrk", reasons = "Known Scammer"},
+	{name = "William Steen", addresses = "email: Willesteen@live.se\nskype: Wille-steen97", nicks = "Wirre", reasons = "Known Scammer"},
+	{name = "Johan Kempe", addresses = "email: jkfan.xd@hotmail.com\nskype: Johaanftw", nicks = "Johaanftw", reasons = "Known Scammer"},
+	{name = "Allex Isaksson", addresses = "email: allexallexallex@live.com\nskype: Forgiven1337", nicks = "Jah", reasons = "Known Scammer"},
+	{name = "-", addresses = "email: czarodziej.czarek@hotmail.com\nskype: czarodziej.czarek", nicks = "Czarek Czarodziej", reasons = "Known Scammer"},
+	{name = "Elizabeth DuBois", addresses = "email: eldubois@gmail.com\nskype: perdo.paulo2", nicks = "Nabirinight", reasons = "Known Scammer"},
+	{name = "-", addresses = "email: Securasolantes12@hotmail.com\nskype: solantes\nskype: Corphia1", nicks = "Solanek, qextibia, corphek, Joe, Exef", reasons = "Known Scammer"},
+	{name = "Henrik Holmgren", addresses = "skype: annesta5\nskype: John123\nskype: Lassermannez", nicks = "-", reasons = "Known Scammer"},
+	{name = "Kamil", addresses = "email: Kamilowsky1337@wp.pl\nskype: SEBEKPAN\nskype: Sbikovvy", nicks = "-", reasons = "Known Scammer"},
+	{name = "-", addresses = "email: turbooman@hotmail.com\nskype: Liveturbooman", nicks = "TurboMan", reasons = "Known Scammer"},
+	{name = "Dominik", addresses = "email: Aerozzcharacters@gmail.com\nskype: dominik10102", nicks = "Aerozz", reasons = "Known Scammer"},
+	{name = "David Johansson", addresses = "email: David.1338@live.se", nicks = "Wantox", reasons = "Known Scammer"},
+	{name = "Marian Puk", addresses = "email: marian1953@gmail.com\nemail: oskar666@outlook.com", nicks = "Oskar", reasons = "Known Scammer"},
+	{name = "Michal Czernecki", addresses = "email: mic.acc@hotmail.com\nsite: http//tibian-gold.weebly.com/\nskype: mic.acc_1", nicks = "Micacc", reasons = "Known Scammer"},
 }
-
 
 -- DO NOT EDIT BELOW --
 
-local VTABLE_PATTERN, tempDuplicated = [[
-[tr]
+local VTABLE_PATTERN1, tempDuplicated, VTABLE_PATTERN2 = [[
+[tr="bgcolor: #cccccc"]
 	[td][B]%d[/B][/td]
 	[td][B]%s[/B][/td]
 	[td][B]%s[/B][/td]
-	[td][B][noparse]%s[/noparse][/B][/td]
 	[td][B]%s[/B][/td]
+	[td][B][noparse]%s[/noparse][/B][/td]
 [/tr]
-]], {}
-
---VTABLE_PATTERN = VTABLE_PATTERN:sub(1, -2)
+]], {}, [[
+[tr="bgcolor: #ffffff"]
+	[td][B]%d[/B][/td]
+	[td][B]%s[/B][/td]
+	[td][B]%s[/B][/td]
+	[td][B]%s[/B][/td]
+	[td][B][noparse]%s[/noparse][/B][/td]
+[/tr]
+]]
 
 setmetatable(SCAM_LIST, {__tostring = function(self)
 	local str = ''
 
 	for i, user in pairs(self) do
-		str = str .. string.format(VTABLE_PATTERN, i, user.nicks, user.reasons, user.adresses, user.name)
+		if i == 1 or i % 2 ~= 0 then
+			str = str .. string.format(VTABLE_PATTERN2, i, user.name, user.nicks, user.reasons, user.addresses, user.reasons)
+		else
+			str = str .. string.format(VTABLE_PATTERN1, i, user.name, user.nicks, user.reasons, user.addresses, user.reasons)
+		end
 	end
 
 	return str
 end})
-
-
--- Sort by name, nick, adresses or reasons
-table.sort(SCAM_LIST, function(a, b)
-	if a.name == b.name then
-		if a.nicks == b.nicks then
-			if a.adresses == b.adresses then
-				return a.reasons < b.reasons
-			else
-				return a.adresses < b.adresses
-			end
-		else
-			return a.nicks < b.nicks
-		end
-	else
-		return a.name < b.name
-	end
-end)
 
 -- Exclude duplicated values
 local index = 1
@@ -91,8 +82,8 @@ while SCAM_LIST[index] do
 	local user = SCAM_LIST[index]
 	local found = false
 
-	for k, v in pairs(tempDuplicated) do
-		if v.name:find(user.name) or v.adresses:find(user.adresses) or v.nicks:find(user.nicks) then
+	for _, v in pairs(tempDuplicated) do
+		if v.name:find(user.name) or v.addresses:find(user.addresses) or v.nicks:find(user.nicks) then
 			found = true
 			break
 		end
@@ -104,6 +95,23 @@ while SCAM_LIST[index] do
 		index = index + 1
 	end
 end
+
+-- Sort by name, nick, addresses or reasons
+table.sort(SCAM_LIST, function(a, b)
+	if a.name == b.name then
+		if a.nicks == b.nicks then
+			if a.addresses == b.addresses then
+				return a.reasons < b.reasons
+			else
+				return a.addresses < b.addresses
+			end
+		else
+			return a.nicks < b.nicks
+		end
+	else
+		return a.name < b.name
+	end
+end)
 
 -- Print out scammers list
 print(SCAM_LIST)
