2e60ff734bb03665e336c5ece674dc19627caeb6
Leonardo Falk <leonardo.falk@hotmail.com>
2014-02-08 22:15:04 -0200

    Update scammerlist.lua
    mixed entries 3 and 7 (same person)
    remove alexis.darks entries
    added extra information for Leicer

--- scammerlist.lua
+++ scammerlist.lua
@@ -37,7 +37,7 @@ SCAM_LIST = {
 	{name = "David Johansson", contact = "email: David.1338@live.se", nicks = "Wantox", reasons = "Known Scammer"},
 	{name = "Marian Puk", contact = "email: marian1953@gmail.com\nemail: oskar666@outlook.com", nicks = "Oskar", reasons = "Known Scammer"},
 	{name = "Michal Czernecki", contact = "email: mic.acc@hotmail.com\nsite: http//tibian-gold.weebly.com/\nskype: mic.acc_1", nicks = "Micacc", reasons = "Known Scammer"},
-	{name = "-", contact = "email: leicer131@gmail.com", nicks = "Leicer", reasons = "Tried to falsificate moderator account to vouch for scam"},
+	{name = "-", contact = "email: leicer131@gmail.com\nskype: leicer4", nicks = "Leicer", reasons = "Tried to falsificate moderator account to vouch for scam"},
 }
 
 -- DO NOT EDIT BELOW --
