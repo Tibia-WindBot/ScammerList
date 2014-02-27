1db79fbbb4aa4a9f0d490d22da2ba70d9182c7cc
Leonardo Falk <leonardo.falk@hotmail.com>
2014-02-27 16:19:59 -0300

    Update scammerlist.lua
    Added "Adrian Urbanski" info

--- scammerlist.lua
+++ scammerlist.lua
@@ -39,6 +39,7 @@ SCAM_LIST = {
 	{name = "Michal Czernecki", contact = "email: mic.acc@hotmail.com\nsite: http//tibian-gold.weebly.com/\nskype: mic.acc_1", nicks = "Micacc", reasons = "Known Scammer"},
 	{name = "-", contact = "email: leicer131@gmail.com\nskype: leicer4", nicks = "Leicer", reasons = "Tried to falsificate moderator account to vouch for scam"},
 	{name = '-', contact = "skype: Rhomez@live.se", nicks = "Izze, Izzeh", reasons = 'Known Scammer'},
+	{name = "Adrian Urbanski", contact = "phone: 512306601", nicks = "Traderosek, Semphor, Everts", reasons = "Stole 550 euros"},
 }
 
 -- DO NOT EDIT BELOW --
