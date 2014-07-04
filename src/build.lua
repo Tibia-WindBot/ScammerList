46dfd6e502e9da667f41d4d00703e1f342539125
Leonardo Falk <leonardo.falk@hotmail.com>
2014-07-04 16:11:42 -0300

    Update 1.2.22a

--- src/build.lua
+++ src/build.lua
@@ -1,48 +1,103 @@
-local ThreadListMaker = {
-	List	=	dofile("TLMlist.lua"),
-	Header	=	dofile("TLMheader.lua"),
-	Line1P	=	dofile("TLMpattern1.lua"),
-	Line2P	=	dofile("TLMpattern2.lua"),
+--[[
+	Scammer List
+	Version 1.2.22
+]]--
+
+local list = {
+	-- Members on the Black List;
+	{name = "Joao Carlos Brinquete", contact = "skype: joao.carlos.brinquete.avo\nemail: joaowbrinquete@hotmail.com\nemail: joaoow@outlook.pt", nicks = "joaoow, iJoaoW", reasons = "Stole 4 Tibia Premium Time"},
+	{name = "Kelli Deferiese", contact = "email: kdefriese@charter.net\nemail: kdefriese@charter.net", nicks = "-", reasons = "Known Scammer"},
+	{name = "Rosa Maria Leal Freitas", contact = "email: rosafreitas@globo.com\nemail:rosafreitas@globo.com", nicks = "-", reasons = "Known Scammer"},
+	{name = "Carlos Mar", contact = "email: silvadomar@globo.com\nemail: silvadomar@globo.com", nicks = "-", reasons = "Known Scammer"},
+	{name = "Marco Ruellas", contact = "email: ruellas@yahoo.com.br\nemail: ruellas@yahoo.com.br", nicks = "-", reasons = "Known Scammer"},
+	{name = "Laura Jones", contact = "email: lawa1985@live.com\nskype: tibia.buyer", nicks = "Flamed", reasons = "Known Scammer"},
+	{name = "Matthew Zwicker", contact = "email: matthewmaclean2000@hotmail.com\nskype: Matthew MacLean", nicks = "Sir Rexy", reasons = "Known Scammer"},
+	{name = "Emil Mortensson", contact = "email: Oscaroggejohansson@hotmail.com", nicks = "Characterforsale, Fiimp, Fimp", reasons = "Known Scammer"},
+	{name = "Jamil Messias Sales", contact = "email: jms.bel@gmail.com -", nicks = "-", reasons = "Known Scammer"},
+	{name = "-", contact = "email: Securasolantes12@hotmail.com\nskype: solantes\nskype: Corphia1\nskype: corphia1\nskype: deidara.1990\nskype: myoncharacters\nemail: solantes12@hotmail.com\nskype: exef.exef", nicks = "corpie, corphek, corphia, Deidara, muooo, Solantes, exefexef, exef.exef, freeusertest1, adi101111, Sonik, exefik, corpha, corphia1, Solanek, qextibia, Joe, Exef", reasons = "Known Scammer"},
+	{name = "Valdecir J. Sousa, Jose De Sousa", contact = "email: vev.valdecirjs@gmail.com", nicks = "-", reasons = "Known Scammer"},
+	{name = "Jaroslaw Sas", contact = "skype: Annacugierkotka\nemail: neobot111@o2.pl", nicks = "amel13141", reasons = "Known Scammer"},
+	{name = "Rodrigo Molinari", contact = "skype: rodrigomolinari2", nicks = "81432858", reasons = "Known Scammer"},
+	{name = "John Barkol", contact = "email: holembik@gmail.com\nskype: john.barkol", nicks = "dexik39", reasons = "Known Scammer"},
+	{name = "JORGE R JACOB FH", contact = "email: jorgejacob@gmail.com", nicks = "leonardogomes", reasons = "Known Scammer"},
+	{name = "Kevin Chau", contact = "email: kevin_chau9506@hotmail.com", nicks = "HeHao", reasons = "Known Scammer"},
+	{name = "Cameron Kieffer", contact = "email: sdeisherlpn2011@hotmail.com, kieffer1230", nicks = "xXKEFFERXx", reasons = "Known Scammer"},
+	{name = "David Sadler", contact = "email: davidsadler47@live.com\nskype: williamsjake", nicks = "Azleh", reasons = "Known Scammer"},
+	{name = "Kevin-Raussche", contact = "email: rausche.kevin@googlemail.com\nskype: maciej.czoger\nskype: tibia.inc\nskype: tim.sukovski", nicks = "czoger, Tibiainc, Moneyrainempire", reasons = "Known Scammer"},
+	{name = "Stig Heimro", contact = "email: stighiemro@hotmail.com\nemail: stighiemro@hotmail.com", nicks = "-", reasons = "Known Scammer"},
+	{name = "Phil (Philip) McCafferty", contact = "skype: Olney\nskype: Illinois\nskype: USAownageforhire\nemail: Mainstreamdarkness@gmail.com\nemail: Frostwynd.tibia@gmail.com", nicks = "ungoliant", reasons = "Known Scammer"},
+	{name = "Hazem Shaba", contact = "email: bassam.shaba@hotmail.se\nskype: Bassehunt", nicks = "nachoo123", reasons = "Known Scammer"},
+	{name = "Alex Wilkes", contact = "email: wilks3y@hotmail.co.uk\nskype: iwilks3y", nicks = "wilks3y", reasons = "Known Scammer"},
+	{name = "-", contact = "-", nicks = "Unit", reasons = "Known Scammer"},
+	{name = "Kurdiskop", contact = "skype: kurdiskop.op1", nicks = "Farbror Kurd III", reasons = "Known Scammer"},
+	{name = "Emerson-Queiroz", contact = "email: Emersontrk@yahoo.com.bremersontrk", nicks = "emersontrk", reasons = "Known Scammer"},
+	{name = "William Steen", contact = "email: Willesteen@live.se\nskype: Wille-steen97", nicks = "Wirre", reasons = "Known Scammer"},
+	{name = "Johan Kempe", contact = "email: jkfan.xd@hotmail.com\nskype: Johaanftw", nicks = "Johaanftw", reasons = "Known Scammer"},
+	{name = "Allex Isaksson", contact = "email: allexallexallex@live.com\nskype: Forgiven1337", nicks = "-", reasons = "Known Scammer"},
+	{name = "-", contact = "email: czarodziej.czarek@hotmail.com\nskype: czarodziej.czarek", nicks = "Czarek Czarodziej", reasons = "Known Scammer"},
+	{name = "Elizabeth DuBois", contact = "email: eldubois@gmail.com\nskype: perdo.paulo2", nicks = "Nabirinight", reasons = "Known Scammer"},
+	{name = "Henrik Holmgren", contact = "skype: annesta5\nskype: John123\nskype: Lassermannez", nicks = "-", reasons = "Known Scammer"},
+	{name = "Kamil", contact = "email: Kamilowsky1337@wp.pl\nskype: SEBEKPAN\nskype: Sbikovvy", nicks = "-", reasons = "Known Scammer"},
+	{name = "-", contact = "email: turbooman@hotmail.com\nskype: Liveturbooman", nicks = "TurboMan", reasons = "Known Scammer"},
+	{name = "Dominik", contact = "email: Aerozzcharacters@gmail.com\nskype: dominik10102", nicks = "Aerozz", reasons = "Known Scammer"},
+	{name = "David Johansson", contact = "email: David.1338@live.se", nicks = "Wantox", reasons = "Known Scammer"},
+	{name = "Marian Puk", contact = "email: marian1953@gmail.com\nemail: oskar666@outlook.com", nicks = "Oskar", reasons = "Known Scammer"},
+	{name = "Michal Czernecki", contact = "email: mic.acc@hotmail.com\nsite: http//tibian-gold.weebly.com/\nskype: mic.acc_1", nicks = "Micacc", reasons = "Known Scammer"},
+	{name = "-", contact = "email: leicer131@gmail.com\nskype: leicer4", nicks = "Leicer", reasons = "Tried to falsificate moderator account to vouch for scam"},
+	{name = "Adrian Urbanski", contact = "phone: 512306601", nicks = "Traderosek, Semphor, Everts", reasons = "Stole 550 euros"},
+	{name = "-", contact = "", nicks = "frozenpker", reasons = "Trading non valid WindBot license"},
+	{name = "-", contact = "skype: Ognisty Rasputin", nicks = "Frox, Ognisty, Rasputin, Ognistyred", reasons = "Stole 63kk from douglindo"},
+	
 }
 
-function table.values(self)
-	local vals = {}
+	-- defining headers and patterns;
+local header = [[
+[CENTER][B][SIZE=7][IMG]http://i.imgur.com/zuRnITx.png[/IMG][/SIZE][/B][/CENTER]
 
-	for _, v in pairs(self) do
-		vals[#vals + 1] = v
-	end
+[hr][/hr]
 
-	return vals
-end
+[TABLE="width: 1024, align: center, class: cms_table_sortable"]
+[tr="bgcolor: #000000"]
+	[td="width: 5%%"][I][B][COLOR="#FFFFFF"]# ID[/B][/I]:[/COLOR][/td]
+	[td="width: 15%%"][B][I][COLOR="#FFFFFF"]Real Name[/I][/B]:[/COLOR][/td]
+	[td="width: 50%%"][I][B][COLOR="#FFFFFF"]Known Nickname(s)[/B][/I]:[/COLOR][/td]
+	[td="width: 15%%"][B][I][COLOR="#FFFFFF"]Reason(s)[/I][/B]:[/COLOR][/td]
+	[td="width: 15%%"][B][I][COLOR="#FFFFFF"]Contact[/I][/B]:[/COLOR][/td]
+[/tr]
+%s[/TABLE]
+]]
+local line1 = [[
+[tr="bgcolor: #cccccc"]
+	[td][B]%d[/B][/td]
+	[td][B]%s[/B][/td]
+	[td][B]%s[/B][/td]
+	[td][B]%s[/B][/td]
+	[td][B][noparse]%s[/noparse][/B][/td]
+[/tr]
+]]
+local line2 = [[
+[tr="bgcolor: #ffffff"]
+	[td][B]%d[/B][/td]
+	[td][B]%s[/B][/td]
+	[td][B]%s[/B][/td]
+	[td][B]%s[/B][/td]
+	[td][B][noparse]%s[/noparse][/B][/td]
+[/tr]
+]]
 
-function table.keys(self)
-	local keys = {}
+setmetatable(list, {__tostring = function(self)
+	-- creating output format;
+	local str, temp = '', {}
 
-	for key, _ in pairs(self) do
-		if (type(key) ~= 'number') then
-			keys[#keys + 1] = key
-		end
-	end
-
-	return keys
-end
-
-setmetatable(ThreadListMaker.List, {__tostring = function(self)
-	--// setting the output string as a metamod;
-	local str = ''
-
-	for index, entry in pairs(self) do
-		if index == 1 or index % 2 ~= 0 then
-			str = str .. string.format(ThreadListMaker.Line1P, index, entry.name, entry.nicks, entry.reasons, entry.contact)
-		else
-			str = str .. string.format(ThreadListMaker.Line2P, index, entry.name, entry.nicks, entry.reasons, entry.contact)
-		end
+	for i, e in pairs(self) do
+		str = str .. string.format(i % 2 ~= 0 and line1 or line2, i, e.name, e.nicks, e.reasons, e.contact)
 	end
 
 	return str
 end})
 
-table.sort(ThreadListMaker.List, function(a, b)
+table.sort(list, function(a, b)
+	-- sorting list members;
 	if a.name == b.name then
 		if a.nicks == b.nicks then
 			if a.contact == b.contact then
@@ -59,10 +114,9 @@ table.sort(ThreadListMaker.List, function(a, b)
 end)
 
 local fl = io.open("../output.txt", "w+")
-
 if (fl ~= nil) then
-	--// checks if creating/clearing file was successful;
-	--// if true then write and close file;
-	fl:write(string.format(ThreadListMaker.Header, tostring(ThreadListMaker.List) .. "\n"))
+	-- checks if creating/clearing file was successful;
+	-- then write and close file;
+	fl:write(string.format(header, tostring(list) .. "\n"))
 	fl:close()
 end
